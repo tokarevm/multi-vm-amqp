76233fb9fab3ab7856b93ad3c4a2f2ef01230007
Maxim Tokarev <mackuso@gmail.com>
2017-08-28 13:38:14 +0300

    Added item and trigger for zabbix

---
+++ files/zabbix/zbx_create_item_trigger.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import sys
+from pyzabbix import ZabbixAPI, ZabbixAPIException
+
+expression = "{#{hostname}:rabbitmaxq.last()}>5"
+description = "Rabbitmq queue is too long"
+priority = 4
+
+# The hostname at which the Zabbix web interface is available
+zabbix_server = 'http://localhost/zabbix'
+
+# Enter administrator credentials for the Zabbix Web Frontend
+username = "Admin"
+password = "zabbix"
+host_name = "central"
+
+zapi = ZabbixAPI(zabbix_server)
+zapi.login(username, password)
+
+hosts = zapi.host.get(filter={"host": host_name}, selectInterfaces=["interfaceid"])
+
+name = "test"
+
+if hosts:
+    host_id = hosts[0]["hostid"]
+    print("Found host id {0}".format(host_id))
+    item = zapi.item.get(filter={"hostid": host_id, "key_": 'rabbitmaxq', "output": "extend"})
+    if not item:
+        try:
+            item = zapi.item.create(
+                hostid=host_id,
+                name='Rabbitmq queue length',
+                description='Rabbitmq queue length',
+                key_='rabbitmaxq',
+                type=0,
+                interfaceid=hosts[0]["interfaces"][0]["interfaceid"],
+                value_type=3,
+                delay=10
+            )
+        except ZabbixAPIException as e:
+            print str(e)
+            sys.exit()
+    try:
+        item = zapi.trigger.create(
+            host=host_id,
+            name='Rabbitmq queue treshold exceeded',
+            description='Rabbitmq queue treshold exceeded',
+            status=0,
+            type=0,
+            priority=3,
+            expression='{central:rabbitmaxq.last(0)}>5'
+        )
+    except ZabbixAPIException as e:
+        print str(e)
+        sys.exit()
+
